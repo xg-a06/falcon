099944f79de2bfb6fe3974b718b0a5c75ce8e33a
hxiaoguang <hxiaoguang@infervision.com>
2022-07-05 01:17:44 +0800

    feat: 修改EventListener type

--- packages/viewport/src/hooks/useEvent.ts
+++ packages/viewport/src/hooks/useEvent.ts
@@ -1,18 +1,20 @@
 import { useRef, useCallback, useLayoutEffect } from 'react';
 
-type Callback = (...args: Array<any>) => void;
+export interface EventListener {
+  (evt: Event): void;
+}
 
 // 据说在react18的并发模式下有问题，但是大多数场景应该没问题。
-const useEvent = (fn: Callback) => {
-  const fnRef = useRef<Callback>();
+const useEvent = (fn: EventListener) => {
+  const fnRef = useRef<EventListener>();
 
   useLayoutEffect(() => {
     fnRef.current = fn;
   }, [fn]);
 
-  const cb = useCallback((...args: Array<any>) => {
+  const cb = useCallback((e: Event) => {
     const handler = fnRef.current;
-    return handler && handler(...args);
+    return handler && handler(e);
   }, []);
 
   return cb;
--- packages/viewport/src/hooks/useEventLister.ts
+++ packages/viewport/src/hooks/useEventLister.ts
@@ -1,9 +1,9 @@
 import { useEffect } from 'react';
-import useEvent from './useEvent';
+import useEvent, { EventListener } from './useEvent';
 
 const empty = () => undefined;
 
-const useEventLister = (target: HTMLElement | Window | null | undefined, eventName: string, fn: (e: UIEvent | Event) => void, deps: Array<any> = []) => {
+const useEventLister = (target: HTMLElement | Window | null | undefined, eventName: string, fn: EventListener, deps: Array<any> = []) => {
   const cb = useEvent(fn);
 
   useEffect(() => {
